5faa02759c56b8bc8874e447a4ab87ddedcd788d
unknown <prasetiautama2@.gmail.com>
2022-07-04 18:59:21 +0200

    Add setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='H10Service',
+    version='0.1',
+    packages=['Lib'],
+    url='',
+    license='',
+    author='Prasetia',
+    author_email='',
+    description='',
+    install_requires=["PyQt5"]
+)
