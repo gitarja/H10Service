4ae3aefa5bbed57aa515542db2a64fdc2a99ff32
gitarja <prasetiautama2@gmail.com>
2022-12-05 15:55:23 +0100

    Update

--- start_ecg_only.sh
+++ start_ecg_only.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
-if sudo bluetoothctl -- trust dd:4e:fa:3f:78:00; then
+if sudo bluetoothctl -- trust F9:04:90:1C:DB:0A; then
 
-	if sudo bluetoothctl -- connect dd:4e:fa:3f:78:00; then
+	if sudo bluetoothctl -- connect F9:04:90:1C:DB:0A; then
 		sudo python main.py --ttl False --ECG True
 	fi
 fi
